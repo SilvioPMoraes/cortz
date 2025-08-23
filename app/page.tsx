c0d4e6f1272fd9751da0d3c245ce2de50ca533ea
SilvioPMoraes <silvio.p.moraes@gmail.com>
2025-08-23 10:26:40 -0300

    shads butios

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1 className="md-5 bg-red-900 text-red-300"> ola </h1>
+  return (
+    <>
+      <h1 className="md-5 bg-red-900 text-red-300"> ola </h1>
+      <button className="md-5 bg-slate-600 text-yellow-50">teste</button>
+    </>
+  )
 }
