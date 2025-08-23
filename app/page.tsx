a2d01542ac9da7c2ec73d9a1f5cfcc930c5326e1
SilvioPMoraes <silvio.p.moraes@gmail.com>
2025-08-23 10:57:46 -0300

    teste

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/app/_components/ui/button"
+const tes = "teste"
 export default function Home() {
   return (
     <>
       <h1 className="md-5 bg-red-900 text-red-300"> ola </h1>
-      <button className="md-5 bg-slate-600 text-yellow-50">teste</button>
+      <Button>teste</Button>
     </>
   )
 }
