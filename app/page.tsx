9c505bb8d8f0dffc9376c8f6ffb3bdeaa306db39
SilvioPMoraes <silvio.p.moraes@gmail.com>
2025-08-26 13:01:25 -0300

    Cards show

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,22 @@ import { Divide, SearchIcon } from "lucide-react"
 import Header from "./_components/ui/header"
 import { Input } from "./_components/ui/input"
 import { Button } from "./_components/ui/button"
+import { Badge } from "./_components/ui/badge"
+import { Avatar } from "./_components/ui/avatar"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { AvatarImage } from "@radix-ui/react-avatar"
 
 const Home = () => {
   return (
     <div>
       <Header />
       <div className="p-5">
+        {/* Texto */}
         <h2 className="text-xl font-bold">Olá, Silvio!</h2>
         <p>Segunda-feira, 25 de agosto.</p>
 
+        {/* Busca */}
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="faça sua busca..."></Input>
           <Button>
@@ -19,6 +25,7 @@ const Home = () => {
           </Button>
         </div>
 
+        {/* Imagem */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Agende nos melhores"
@@ -27,6 +34,30 @@ const Home = () => {
             className="rounded-xl object-cover"
           />
         </div>
+
+        {/* Agendamento */}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* Esquerda */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"></AvatarImage>
+                </Avatar>
+                <p className="text-sm">Barbearia teste</p>
+              </div>
+            </div>
+
+            {/* Direita */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
