a50571ace1256abd0b38c28b41894273fe5f24b4
SilvioPMoraes <silvio.p.moraes@gmail.com>
2025-08-29 13:12:37 -0300

    Add Book

---
+++ app/_constants/search.ts
@@ -0,0 +1,8 @@
+export const quickSearchOptions = [
+  { id: 1, imageUrl: "/cabelo.svg", title: "Cabelo" },
+  { id: 2, imageUrl: "/barba.svg", title: "Barba" },
+  { id: 3, imageUrl: "/acabamento.svg", title: "Acabamento" },
+  { id: 4, imageUrl: "/massagem.svg", title: "Massagem" },
+  { id: 5, imageUrl: "/sobrancelha.svg", title: "Sobrancelha" },
+  { id: 6, imageUrl: "/hidratacao.svg", title: "Hidratação" },
+]
--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,17 @@ import { SearchIcon } from "lucide-react"
 import Header from "./_components/ui/header"
 import { Input } from "./_components/ui/input"
 import { Button } from "./_components/ui/button"
-import { Badge } from "./_components/ui/badge"
-import { Avatar } from "./_components/ui/avatar"
 import createApiInstance from "./_constants/api.js"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
-import { AvatarImage } from "@radix-ui/react-avatar"
 import React, { useEffect, useState } from "react"
 import BarbershopItem from "./_components/barbershop-item"
+import BookingItem from "./_components/booking-item"
+import { quickSearchOptions } from "./_constants/search"
 
 const Home = () => {
   const [barbershop, setBarbershop] = useState([])
+  const [barbershoppopular, setBarbershoppopular] = useState([])
 
   //const barbershops = await fetchData()
   //console.log(barbershops)
@@ -72,6 +72,51 @@ const Home = () => {
     }
   }
 
+  async function BarberPupular() {
+    try {
+      //console.log("function fetchData")
+      //setLoading(true);
+
+      const api = await createApiInstance()
+
+      const authHeader = "Basic " + btoa("SysT@xi:27021970") // Substitua com suas credenciais
+
+      const data = {
+        //cpf: user.cpf,
+        //situacao: "F",
+      }
+
+      const response = await api.get("/barbeariapopular", {
+        params: data, // Dados como parâmetros de URL
+        headers: {
+          Authorization: authHeader, // Cabeçalho de autenticação
+        },
+      })
+
+      //setLoading(false);
+      //setBarbershop(response.data)
+      //console.log(response.data)
+      setBarbershoppopular(response.data)
+
+      //setLista( response.data );
+      //setModalText(response.data);
+      //showOkModal();
+    } catch (error) {
+      //setLoading(false);
+      console.log(error)
+
+      //      if (error.response?.data.error) {
+      //      setModalType('ok'); // Modal com "Sim" e "Não"
+      //    setModalText(error.response.data.error);
+      //  setModalVisible(true);
+      //    } else {
+      //setModalType('ok'); // Modal com "Sim" e "Não"
+      //setModalText("Ocorreu um erro, tente novamente mais tarde...");
+      //setModalVisible(true);;
+      //  }
+    }
+  }
+
   // useEffect para chamar a API ao carregar a tela
   //useEffect(() => {
   //const getData = async () => {
@@ -83,6 +128,7 @@ const Home = () => {
   //}, [])
 
   fetchData()
+  BarberPupular()
 
   console.log(barbershop)
 
@@ -102,7 +148,22 @@ const Home = () => {
           </Button>
         </div>
 
-        {/* Imagem */}
+        {/* BUSCA RAPIDA */}
+        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          {quickSearchOptions.map((option) => (
+            <Button className="gap-2" variant="secondary" key={option.title}>
+              <Image
+                alt={option.title}
+                src={option.imageUrl}
+                width={16}
+                height={16}
+              />
+              {option.title}
+            </Button>
+          ))}
+        </div>
+
+        {/* IMAGEM */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Agende nos melhores"
@@ -112,34 +173,10 @@ const Home = () => {
           />
         </div>
 
-        {/* Agendamento */}
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            {/* Esquerda */}
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"></AvatarImage>
-                </Avatar>
-                <p className="text-sm">Barbearia teste</p>
-              </div>
-            </div>
+        {/* AGENDAMENTO */}
+        <BookingItem />
 
-            {/* Direita */}
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">05</p>
-              <p className="text-sm">20:00</p>
-            </div>
-          </CardContent>
-        </Card>
-
-        {/* Recomendados */}
+        {/* RECOMENDADOS */}
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
         </h2>
@@ -148,7 +185,27 @@ const Home = () => {
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
+
+        {/* POPULARES */}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Populares
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barbershoppopular.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
+
+      <footer>
+        <Card>
+          <CardContent className="px-5 py-6">
+            <p className="text-sm text-gray-400">
+              2025 Copyright <span className="font-bold">FWS Barber</span>
+            </p>
+          </CardContent>
+        </Card>
+      </footer>
     </div>
   )
 }
